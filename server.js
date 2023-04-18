7696b198ccd34a18ea5cc4d7a5356c5b60747602
xing-6464 <xing_6464@foxmail.com>
2023-04-18 17:44:50 +0800

    请求函数构造

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // 导入http模块
 const http = require('http');
+const fs = require('fs');
 
 // 创建服务器
 const server = http.createServer();
@@ -10,9 +11,16 @@ server.listen(8081, () => {
 });
 
 server.on('request', (req, res) => {
-  console.info('555');
+  // console.info('555');
 
-  res.setHeader('Content-Type', 'text/html;charset=utf8');
-  res.write('<h1>你好</h1>');
-  res.end();
+  // res.setHeader('Content-Type', 'text/html;charset=utf8');
+  // res.write('<h1>你好</h1>');
+  // res.end();
+
+  if (req.url == '/') {
+    fs.readFile('./index.html', 'utf-8', (err, data) => {
+      res.write(data);
+      res.end();
+    });
+  }
 });
